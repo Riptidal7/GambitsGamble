a933e6c4db638e1b41f71acbb8333287db21dd50
Tim Deutschman <timdeu05@gmail.com>
2025-04-13 13:46:48 -0700

    Balanced Heal Spell
    Level 1 now heals for 2 and Level 2 heals for 4

--- Assets/Scripts/GameParameters.cs
+++ Assets/Scripts/GameParameters.cs
@@ -36,12 +36,12 @@ public static class GameParameters
 
     public static int FireSpell1DFlatDamage = 5;
     public static int IceSpell1FlatDamage = 5;
-    public static int HealSpell1Heal = 1;
+    public static int HealSpell1Heal = 2;
     public static int MeleeAttackDamage = 3;
     
     public static int FireSpell2DFlatDamage = 7;
     public static int IceSpell2FlatDamage = 7;
-    public static int HealSpell2Heal = 2;
+    public static int HealSpell2Heal = 4;
     
     public static int FireSpell3DFlatDamage = 10;
     public static int IceSpell3FlatDamage = 10;
