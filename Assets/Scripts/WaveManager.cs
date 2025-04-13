916a1759d08dd594a9bd0d3455c4152c8ede8875
Tim Deutschman <timdeu05@gmail.com>
2025-04-13 11:41:59 -0700

    Adjusted Waves
    Waves now scale at a rate that feels manageable

--- Assets/Scripts/WaveManager.cs
+++ Assets/Scripts/WaveManager.cs
@@ -6,7 +6,7 @@ using Random = UnityEngine.Random;
 public  class WaveManager : MonoBehaviour
 {
     public  int waveNumber;
-    public bool AddMob2s;
+    //public bool AddMob2s;
     public int MaxEnemies;
     public EnemyWave CurrentWave = new EnemyWave();
     public PowerUpChoiceMenu powerUpChoiceMenu;
@@ -16,24 +16,24 @@ public  class WaveManager : MonoBehaviour
     void Start()
     {
         waveNumber = 1;
-        AddMob2s = false;
+        //AddMob2s = false;
         waveClearedSwitch = false;
         GenerateNewWave();
     }
     public void GenerateNewWave()
     {
         print("new wave");
-        int numbSlimes = Random.Range(GameParameters.MinNumberSlimesPerWave, GameParameters.MaxNumberSlimesPerWave+1) + waveNumber;
+        int numbSlimes = Random.Range(GameParameters.MinNumberSlimesPerWave, GameParameters.MaxNumberSlimesPerWave+1)/2 + waveNumber;
         int numbMob2s = 0;
-        if(AddMob2s)
+        if(waveNumber % 3 == 0)
         {
-            numbMob2s = Random.Range(GameParameters.MinNumberMob2sPerWave, GameParameters.MaxNumberMob2sPerWave+1) + waveNumber-1;
-            AddMob2s = false;
+            numbMob2s = waveNumber/3;
+            //AddMob2s = false;
         }
         else
         {
-            AddMob2s = true;
-            numbMob2s = waveNumber - 1;
+            //AddMob2s = true;
+            //numbMob2s = waveNumber - 1;
         }
         CurrentWave.CreateNewWave(numbSlimes,numbMob2s);
         waveNumber++;
