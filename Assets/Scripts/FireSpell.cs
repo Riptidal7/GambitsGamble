70690c928e5d0aa169238ee7264799bb21fce3ea
Tim Deutschman <timdeu05@gmail.com>
2025-05-12 20:35:43 -0700

    Fixed Spell Targeting
    Spells inheriting from SpellParent can now hit multiple enemies at one! Also made changes to the classes that inherit, because the collision boolean is no longer a part of spell parent, and it checks collision for spell effects by adding to a list instead

--- Assets/Scripts/FireSpell.cs
+++ Assets/Scripts/FireSpell.cs
@@ -1,89 +1,13 @@
-using System;
-using System.Buffers.Text;
-using System.Collections;
-using UnityEngine;
-
 public class FireSpell : SpellParent
 {
-    //NOTE BURNING DOES NOT EFFECT STRONGER PUPRLE MOB YET
-    void Start()
+    protected override void Start()
     {
         SpellFlatDamage = GameParameters.FireSpell1DFlatDamage;
-        
         base.Start();
-
     }
 
-    void Update()
+    protected override void OnEnemyHit(Enemy enemy)
     {
-        if  (hasCollided)
-        {
-            enemy.isBurning = true;
-        }
+        enemy.isBurning = true;
     }
-    
-    
-
 }
-   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    //==============================================================
-    // everything below is commented and added to SpellParent class 
-    //==============================================================
-
-
-    /* void Start()
-     {
-
-         StartCoroutine(CountdownUntilDisappear());
-     }
-
-
-
-
-
-     public void OnTriggerEnter2D(Collider2D other)
-     {
-         if (other.gameObject.tag == "Slime")
-         {
-             other.gameObject.GetComponent<Slime>().HitPoints -= GameParameters.FireSpell1DFlatDamage;
-             other.gameObject.GetComponent<Slime>().isBurning = true;
-
-         }
-
-         if (other.gameObject.tag == "Mob2")
-         {
-             other.gameObject.GetComponent<Slime1>().HitPoints -= GameParameters.FireSpell1DFlatDamage;
-             other.gameObject.GetComponent<Slime1>().isBurning = true;
-         }
-     }
-
-     IEnumerator CountdownUntilDisappear()
-     {
-         yield return new WaitForSeconds(1);
-         GameObject.Destroy(gameObject);
-     }
-     */
-
