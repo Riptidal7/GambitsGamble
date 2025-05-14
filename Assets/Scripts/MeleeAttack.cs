07c65818f6fab2744d531c571b9b05bb6002c5a1
Li <lihong@chapman.edu>
2025-05-14 15:16:21 -0700

    working knockback implemented!
    theres a null error getting thrown but game works. empty projectile to be populated soon.

--- Assets/Scripts/MeleeAttack.cs
+++ Assets/Scripts/MeleeAttack.cs
@@ -3,39 +3,71 @@ using UnityEngine;
 
 public class MeleeAttack : MonoBehaviour
 {
+	public KnockbackFeedback knockbackFeedback;  // Only keep this one reference for KnockbackFeedback
+	public Player Gambit;
     
 	//public Color defaultColor;
 
     void Start()
     {
+		// Ensure Gambit is properly assigned
+	    Gambit = GameObject.FindWithTag("Player").GetComponent<Player>();
 
         StartCoroutine(CountdownUntilDisappear());
     }
     
     public void OnTriggerEnter2D(Collider2D other)
     {
-        SFXManager.Play("EnemyTakesMeleeDamage");
-        if (other.gameObject.tag == "Slime")
-        {
-			other.gameObject.GetComponent<Slime>().HitPoints -= GameParameters.MeleeAttackDamage;
+	    //intialize knockback variables to be changed in method later
+	    DirectionType direction = Gambit.direction;
+	    Vector2 knockbackDirection = Vector2.zero;
 
-			if (other.gameObject.GetComponent<Slime>().HitPoints > 0)
+        SFXManager.Play("EnemyTakesMeleeDamage");
+        if (other.gameObject.CompareTag("Slime"))
+        {
+	        Slime slime = other.gameObject.GetComponent<Slime>();
+	        slime.HitPoints -= GameParameters.MeleeAttackDamage;
+	        
+	        KnockbackFeedback slimeKnockback = slime.GetComponent<KnockbackFeedback>();
+	        if (slimeKnockback != null)
+	        {
+		        slimeKnockback.AssignKnockbackDirection(direction, ref knockbackDirection);  // Pass by reference
+		        Debug.Log("Applying knockback to Slime in direction: " + knockbackDirection);
+		        slimeKnockback.ApplyKnockback(direction, knockbackDirection);
+	        }
+	        else
+		        Debug.LogWarning("No KnockbackFeedback component found on the Slime!");
+
+
+			if (slime.HitPoints > 0)
 			{
 				SpriteRenderer spriteRenderer = other.gameObject.GetComponent<SpriteRenderer>();
-				Color defaultColor = other.gameObject.GetComponent<Slime>().defaultColor;
+				Color defaultColor = slime.defaultColor;
 				StartCoroutine(CountdownForDamageIndicator(spriteRenderer, defaultColor));
 			}
 			
         }
         
-        if (other.gameObject.tag == "Mob2")
+        if (other.gameObject.CompareTag("Mob2"))
         {	
-			other.gameObject.GetComponent<Slime1>().HitPoints -= GameParameters.MeleeAttackDamage;
+	        Slime1 slime1 = other.gameObject.GetComponent<Slime1>();
+	        slime1.HitPoints -= GameParameters.MeleeAttackDamage;
 
-			if (other.gameObject.GetComponent<Slime>().HitPoints > 0)
+	        // Get KnockbackFeedback component from the Slime1
+	        KnockbackFeedback slime1Knockback = slime1.GetComponent<KnockbackFeedback>();
+	        if (slime1Knockback != null)
+	        {
+		        slime1Knockback.AssignKnockbackDirection(direction, ref knockbackDirection);  // Pass by reference
+		        Debug.Log("Applying knockback to Slime1 in direction: " + knockbackDirection);
+		        slime1Knockback.ApplyKnockback(direction, knockbackDirection);
+	        }
+	        else
+		        Debug.LogWarning("No KnockbackFeedback component found on the Slime1!");
+	        
+			if (slime1.HitPoints > 0)
 			{
 				SpriteRenderer spriteRenderer = other.gameObject.GetComponent<SpriteRenderer>();
-				Color defaultColor = other.gameObject.GetComponent<Slime1>().defaultColor;
+				Color defaultColor = slime1.defaultColor;
 				StartCoroutine(CountdownForDamageIndicator(spriteRenderer, defaultColor));
 			}
 			
