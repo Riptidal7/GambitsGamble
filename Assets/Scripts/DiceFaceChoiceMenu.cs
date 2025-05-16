ac106da78962e934740f130f269582c426d11116
mayafederico <maya.federico8@gmail.com>
2025-05-16 14:45:22 -0700

    Finished Tutorial
    (thank you charlotte!!)

--- Assets/Scripts/DiceFaceChoiceMenu.cs
+++ Assets/Scripts/DiceFaceChoiceMenu.cs
@@ -14,6 +14,8 @@ public class DiceFaceChoiceMenu : MonoBehaviour
     public List<Image> diceFaceImages;
     public List<string> currentSpellSlots;
     public string spellToBeApplied;
+	public bool isInTutorial=false;
+	public bool hasSelectedSpell=false;
     
     //dictionary that connects spell names to sprites and actions
     public Dictionary<string, Sprite> spellSprites; 
@@ -82,42 +84,60 @@ public class DiceFaceChoiceMenu : MonoBehaviour
         };
     }
 
+	public void TutorialRollDie()
+	{
+		int spellSlot = 0;
+		for (int i = 0; i < 6; i++) 
+		{
+			if (currentSpellSlots[i] == "Fire Spell")
+			{
+				spellSlot = i;
+			}
+		}
+		
+	}
+
     
     //Li: i heavily crutched on ChatGPT for the methods below!! need to learn what they ACTUALLY do lol
     public void OnFaceChoiceClicked(int slotIndex)
-{
-    
-    if (HasBeenClicked)
-        return;
+	{
+    	if(isInTutorial)
+		{
+			TutorialOnFaceChoiceClicked(slotIndex);
+		}
+  	 	else if (HasBeenClicked)
+      		return;
+		else
+		{
     // Determine the spell to be applied and the corresponding spell level (standard or level 2)
-    string spellKey = spellToBeApplied;
-    string spellKey2 = spellToBeApplied + "2"; // For "Fire Spell2", "Ice Spell2", "Heal Spell2"
+ 	   		string spellKey = spellToBeApplied;
+  		  	string spellKey2 = spellToBeApplied + "2"; // For "Fire Spell2", "Ice Spell2", "Heal Spell2"
 
     // Check if the current slot already contains the selected spell
-    bool isSameSpell = currentSpellSlots[slotIndex] == spellToBeApplied;
+   		 	bool isSameSpell = currentSpellSlots[slotIndex] == spellToBeApplied;
 
     // Determine which sprite to use based on whether it's the same spell or not
-    Sprite selectedSprite = isSameSpell ? spellSprites[spellKey2] : spellSprites[spellKey];
+    		Sprite selectedSprite = isSameSpell ? spellSprites[spellKey2] : spellSprites[spellKey];
 
     // Determine the correct spell cast action
-    Action spellCastAction = isSameSpell ? spellActions[spellKey2] : spellActions[spellKey];
+ 	   		Action spellCastAction = isSameSpell ? spellActions[spellKey2] : spellActions[spellKey];
 
     // Update the dice face sprite in the appropriate slot
-    diceFaceImages[slotIndex].sprite = selectedSprite;
+ 	   		diceFaceImages[slotIndex].sprite = selectedSprite;
 
     // Update the diceRoller DieRollSprites array with the correct sprite for the selected slot
-    diceRoller.DieRollSprites[slotIndex] = selectedSprite;
+ 	   		diceRoller.DieRollSprites[slotIndex] = selectedSprite;
 
     // Set the corresponding spell casting action for the correct slot
-    AssignSlotCastAction(slotIndex, spellCastAction);
+ 	   		AssignSlotCastAction(slotIndex, spellCastAction);
 
     // Update the current spell slot for the selected index
-    currentSpellSlots[slotIndex] = spellToBeApplied;
+ 		   currentSpellSlots[slotIndex] = spellToBeApplied;
 
     // Delay and hide the menu after the selection
-    StartCoroutine(MenuSelectDelay());
-    
-}
+   		 	StartCoroutine(MenuSelectDelay());
+    	}
+	}
 
 void AssignSlotCastAction(int slotIndex, Action spellCastAction)
 {
@@ -159,6 +179,13 @@ void AssignSlotCastAction(int slotIndex, Action spellCastAction)
         powerUpChoiceMenu.ResumeGameAfterAllChoices();
     }
 
+	public void TutorialHideDiceFaceChoiceMenu()
+    {
+        CanvasGroupDisplayer.Hide(DiceFaceChoiceMenuPanel);
+        powerUpChoiceMenu.ResumeGameAfterAllChoices();
+		hasSelectedSpell=true;
+    }
+
     private void StartHideDiceFaceChoiceMenu()
     { 
         CanvasGroupDisplayer.Hide(DiceFaceChoiceMenuPanel);
@@ -173,5 +200,69 @@ void AssignSlotCastAction(int slotIndex, Action spellCastAction)
         SFXManager.Play("Boon");
         HasBeenClicked = false;
     }
+
+	public void TutorialOnFaceChoiceClicked(int slotIndex)
+	{
+    
+    	if (HasBeenClicked)
+    	    return;
+    // Determine the spell to be applied and the corresponding spell level (standard or level 2)
+    	string spellKey = spellToBeApplied;
+   	 	string spellKey2 = spellToBeApplied + "2"; // For "Fire Spell2", "Ice Spell2", "Heal Spell2"
+
+    // Check if the current slot already contains the selected spell
+	    bool isSameSpell = currentSpellSlots[slotIndex] == spellToBeApplied;
+
+    // Determine which sprite to use based on whether it's the same spell or not
+	    Sprite selectedSprite = isSameSpell ? spellSprites[spellKey2] : spellSprites[spellKey];
+
+    // Determine the correct spell cast action
+	    Action spellCastAction = isSameSpell ? spellActions[spellKey2] : spellActions[spellKey];
+
+    // Update the dice face sprite in the appropriate slot
+	    diceFaceImages[slotIndex].sprite = selectedSprite;
+		/*
+		for (int i = 0; i < diceFaceImages.length; i++)	
+		{
+			diceFaceImages[i].sprite = selectedSprite;
+		}
+		*/
+
+    // Update the diceRoller DieRollSprites array with the correct sprite for the selected slot
+	    //diceRoller.DieRollSprites[slotIndex] = selectedSprite;
+		for (int i = 0; i < 6; i++)	
+		{
+			diceRoller.DieRollSprites[i] = selectedSprite;
+		}
+
+    // Set the corresponding spell casting action for the correct slot
+	    //AssignSlotCastAction(slotIndex, spellCastAction);
+		for (int i = 0; i < 6; i++)
+		{
+			AssignSlotCastAction(i, spellCastAction);
+		}
+
+    // Update the current spell slot for the selected index
+	    //currentSpellSlots[slotIndex] = spellToBeApplied;
+		for (int i = 0; i < 6; i++)
+		{
+			currentSpellSlots[i] = spellToBeApplied;
+		}
+
+    // Delay and hide the menu after the selection
+	    StartCoroutine(TutorialMenuSelectDelay());
+    
+	}
+
+	IEnumerator TutorialMenuSelectDelay()
+    {
+        HasBeenClicked = true;
+        yield return new WaitForSeconds(1);
+        TutorialHideDiceFaceChoiceMenu();
+        
+        SFXManager.Play("Boon");
+        HasBeenClicked = false;
+    }
+
     
 }
